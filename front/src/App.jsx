13b168f9f2e3b57a44b4469124a4360923ffc586
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-07-25 15:59:32 -0300

    [FEAT] chat arrumado

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Index from './componentes/Index/Index'
+import Chat from './componentes/Chat/Chat'
 import Signin from './componentes/Signin/Signin'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Signin />
+    <Chat />
   </React.StrictMode>,
 )
--- front/src/componentes/Chat/Chat.jsx
+++ front/src/componentes/Chat/Chat.jsx
@@ -1,27 +1,53 @@
 import React, { useEffect, useState } from "react";
+import './Chat.css';
 
 function Chat({ id }) {
-    const [questions, setQuestions] = useState([
-        {question: "Olá ${} como podemos ajudar?", options: ["Buscar informações de usuário", "Buscar informações de transações", "Buscar informações de conta"]}
-        {question: "Como deseja selecionar?", options: ["Por Email", "Por Telefone", "Por Identificador"]}
+    const [questions] = useState([
+        {question: "Olá, tudo bem ?"},
+        {question: "Como podemos te ajudar ?", options: ["Informações de transação", "Informações de conta", "Informações de perfil"]},
+        {question: "Informe-nos seu cpf"},
+        {question: "Informe-nos seu email"}
     ]);
     const [response, setResponse] = useState();
     
     useEffect(() => {
+        document.getElementById("ChatMessages").innerHTML = `<div class="containerBotMessage"> <p class="botMessage" id="bot">${questions[0].question}</p> </div>`;
+        setTimeout(() => {
+            document.getElementById("ChatMessages").innerHTML += `<div class="containerBotMessage"> <p class="botMessage" id="bot">${questions[1].question}</p> </div>`;
+        }, 1000);
 
-        fetch(`http://localhost:5050/chat/findMessages/${id}`)
-        .then()
-        .catch()
-    }, [response])
+        // fetch(`http://localhost:5050/chat/findMessages/${id}`)
+        // .then()
+        // .catch()
+    }, [])
+
+    const sendMessage = (event) => {
+        console.log('Event:: ' + event);
+        document.getElementById("ChatMessages").innerHTML += `<div class="containerClientMessage"> <p class="clientMessage" id="client">${response}</p> </div>`;
+    }
+
 
     return (
         <div className="containerChat">
-            {
-                selectOthers
-            }
-            {
-                selectMyDB
-            }
+            <div
+                id="ChatMessages" 
+                className="containerChatMessages"
+            >
+            </div>
+            <div className="containerChatInput">
+                <input
+                    type="text"
+                    id="inputChat"
+                    className="inputChat"
+                    onChange={e => setResponse(e.target.value)}
+                />
+                <button
+                    className="btnChat"
+                    onClick={sendMessage}
+                >
+                    <i className="bi-arrow-right-short"></i>
+                </button>
+            </div>
         </div>
     )    
 }
